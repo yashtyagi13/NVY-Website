e296dfb38254dfdaaa17194a55d94c1bc170bf10
yashtyagi13 <yashtyaiiitl332@gmail.com>
2025-05-01 12:05:57 +0530

    3rd component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Home from '../../components/Home/page'
 import AboutUsSection from '../../components/Contact-Us/page'
-// import TeamSection from '../../components/Team Members/page'
+import TeamSection from '../../components/Team Members/page'
 
 function page() {
   return (
     <div>
       <Home/>
       <AboutUsSection/>
-      {/* <TeamSection/> */}
+      <TeamSection/>
     </div>
   )
 }
