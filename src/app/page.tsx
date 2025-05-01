ffcd7a6e0da280485403a315c38ffeadf8753ad1
yashtyagi13 <yashtyaiiitl332@gmail.com>
2025-05-01 11:54:00 +0530

    Few changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from '../../components/Home/page'
 import AboutUsSection from '../../components/Contact-Us/page'
-import TeamSection from '../../components/Team Members/page'
+// import TeamSection from '../../components/Team Members/page'
 
 function page() {
   return (
